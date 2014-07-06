4e1c2a39e00647710d556d270c73d75079269e91
Marcin TORGiren Fabrykowski <torgiren@gmail.com>
2014-07-06 05:17:08 +0200

    liczymy fibbonaciego ?fib=5

--- index.php
+++ index.php
@@ -1,4 +1,16 @@
 <?php
+function fibonacci ($n)
+{
+	if ($n == 1 || $n == 2)
+	{
+		return 1;
+	}
+	else
+	{
+		return fibonacci( $n - 1)+fibonacci( $n - 2 );
+	}
+}
 echo "a ja placeholer gla mgr_test1";
-sleep(1);
+if(isset($_GET['fib']))
+	echo fibonacci($_GET['fib']);
 ?>
